b761e9abf284dd503658fc31735bf45cb9d1a09b
Matthew Brooks <mdeanbrooks@gmail.com>
2015-08-01 18:18:10 -0700

    Implement non-working parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,30 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  // your code goes here
+var parseJSON = function (string) {
+  var current = null;
+  var next = function(start, end) {
+    current = string.slice(start, end);
+  };
+  var result = null;
+  if(string.charAt(1) === "[") {
+    result = [];
+  } else {
+    result = {}; 
+  }
+  var numPattern = /^\d+\.?\d*/;
+  var stringPattern = /\".+\"/;
+  if(numPattern.test(current)) {
+    return Number(current);
+  }
+  if(stringPattern.test(current)) {
+
+  }
+  if(whiteSpace.test(current)) {
+    
+  }
 };
+
+
+
+
